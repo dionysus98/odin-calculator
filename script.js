d1c67271bcd132060420ba899647b802a3bff371
Avinash S <avinashthebigboss@gmail.com>
2021-09-25 20:08:21 +0530

    EDITED index.html style.css script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const layoutThree = document.getElementById('layout__three');
 const displayMini = document.getElementById('display__mini');
 const displayLarge = document.getElementById('display__large');
 
-const calcButtons = document.getElementById('buttons');
 
 burgerMenu.addEventListener('click', function () {
     layoutOne.classList.toggle('layout__one__active');
@@ -27,53 +26,91 @@ colorMenu.addEventListener('click', function () {
 const result = {
     resultMini: [],
     resultLarge: [],
+    allValue: []
 }
 
 
 window.addEventListener('keydown', function (e) {
+
     const dataKeys = document.querySelector(`button[data-key = "${e.keyCode}"]`);
     dataKeys.classList.add('btn__active');
-
     // console.log(dataKeys.textContent);
     if ((displayLarge.textContent).length < 20) {
         displayLarge.textContent += dataKeys.textContent;
     }
-    resultLarge.push(Number(displayLarge.textContent));
-
 
 })
+result['resultLarge'].push(Number(displayLarge.textContent));
+console.log(result);
 
 window.addEventListener('keydown', function (e) {
     const operatorKeys = document.querySelector(`button[operator-key = "${e.keyCode}"]`);
     operatorKeys.classList.add('btn__active');
     if (operatorKeys.textContent === "+") {
+
         if ((displayMini.textContent).length < 50) {
-            displayMini.textContent += (displayLarge.textContent + ' + ');
+            displayMini.textContent += (Number(displayLarge.textContent) + ' +');
         }
+
         displayLarge.textContent = '';
     } else if ((operatorKeys.textContent === "-")) {
         if ((displayMini.textContent).length < 50) {
-            displayMini.textContent += (displayLarge.textContent + ' - ');
+            displayMini.textContent += (Number(displayLarge.textContent) + ' -');
         }
         displayLarge.textContent = '';
     } else if ((operatorKeys.textContent === "*")) {
+
         if ((displayMini.textContent).length < 50) {
-            displayMini.textContent += (displayLarge.textContent + ' * ');
+            displayMini.textContent += (Number(displayLarge.textContent) + ' *');
         }
         displayLarge.textContent = '';
     } else if ((operatorKeys.textContent === "/")) {
+
         if ((displayMini.textContent).length < 50) {
-            displayMini.textContent += (displayLarge.textContent + ' / ');
+            displayMini.textContent += (Number(displayLarge.textContent) + ' /');
         }
         displayLarge.textContent = '';
     }
+
+    if (operatorKeys.textContent === "=") {
+        if ((displayMini.textContent).length < 50) {
+            displayMini.textContent += (Number(displayLarge.textContent));
+        }
+        // displayLarge.textContent = '';
+
+        result['resultMini'].push((displayMini.textContent));
+
+        let localMemory = result.resultMini.pop();
+        // console.log(localMemory);
+
+        let transformValue = (localMemory.replaceAll(/[^\w\s]/g, ','));
+        // console.log(local, local.length, typeof local);
+
+        let arr = transformValue.split(',');
+        console.log(arr);
+
+        result['allValue'].push(arr);
+
+        // let math = 0;
+
+        // for (let i = 0; i < arr.length; i++) {
+
+        //     math = math 
+
+        // }
+
+
+    }
+    // result['resultMini'].push((displayMini.textContent));
+
 })
+
+
 
 
 window.addEventListener('keydown', function (e) {
     const displayKeys = document.querySelector(`button[display-key = "${e.keyCode}"]`);
     displayKeys.classList.add('btn__active');
-
     console.log(displayKeys.textContent);
     if (displayKeys.textContent === "Clear") {
         displayLarge.textContent = '';
@@ -86,8 +123,12 @@ window.addEventListener('keydown', function (e) {
 
 
 
+
+
 const keysTransform = Array.from(document.querySelectorAll('.btn'));
 keysTransform.forEach(key => key.addEventListener('transitionend', function (event) {
     if (event.propertyName !== 'transform') return;
     event.target.classList.remove('btn__active');
 }))
+
+
